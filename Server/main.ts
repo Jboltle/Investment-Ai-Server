8d261cf6cba5176c3a3cd21bdd1ffde265301067
Jboltle <stacbolt2@gmail.com>
2025-04-11 13:40:02 -0400

    removed hardcoded port

--- Server/main.ts
+++ Server/main.ts
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 1000;
 
 // Define allowed origins
 const allowedOrigins = [
