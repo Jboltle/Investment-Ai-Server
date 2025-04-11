7afe4168d6f8285b9f29faee8950b6508a9a650e
Jboltle <stacbolt2@gmail.com>
2025-04-11 14:35:20 -0400

    ports

--- Server/main.ts
+++ Server/main.ts
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '10000');
 
 // Define allowed origins
 const allowedOrigins = [
